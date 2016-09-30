a48ba877864b19087b93463b696b339586ca2d1f
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-09-30 15:12:34 -0700

    Menu item routes

---
+++ routes/menuItems.js
@@ -0,0 +1,15 @@
+var express = route('express');
+var router = express.Router();
+
+var controller = require('../controllers/menuItems.js');
+
+router.route('/')
+  .get(controller.index)
+  .post(controller.create);
+
+router.route('/:id')
+  .get(controller.show)
+  .patch(controller.update)
+  .delete(controller.delete);
+
+module.exports = router;
